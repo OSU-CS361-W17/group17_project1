cc67e875a734b5803eec869d5494c6747a5c30e4
GarrettHaley <haleyg@oregonstate.edu>
2017-01-23 21:53:48 -0800

    Create Destroyer.java

---
+++ src/main/java/edu/oregonstate/cs361/battleship/Destroyer.java
@@ -0,0 +1,38 @@
+public class Destroyer{
+
+private String name;
+private Integer length;
+private String start;
+private String end;
+
+public String getName() {
+return name;
+}
+
+public void setName(String name) {
+this.name = name;
+}
+
+public Integer getLength() {
+return length;
+}
+
+public void setLength(Integer length) {
+this.length = length;
+}
+
+public Start getStart() {
+return start;
+}
+
+public void setStart(String start) {
+this.start = start;
+}
+
+public End getEnd() {
+return end;
+}
+
+public void setEnd(String end) {
+this.end = end;
+}
