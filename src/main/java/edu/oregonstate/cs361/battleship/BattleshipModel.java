810775ee2435fb366cfcf5ad9d985352c0c39a3f
GarrettHaley <haleyg@oregonstate.edu>
2017-01-23 21:47:00 -0800

    Update BattleshipModel.java

--- src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
+++ src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
@@ -2,6 +2,39 @@ package edu.oregonstate.cs361.battleship;
 
 public class BattleshipModel {
 
-return null;
+private String name;
+private Integer length;
+private String start;
+private String end;
 
+public String getName() {
+return name;
+}
+
+public void setName(String name) {
+this.name = name;
+}
+
+public Integer getLength() {
+return length;
+}
+
+public void setLength(Integer length) {
+this.length = length;
+}
+
+public Start getStart() {
+return start;
+}
+
+public void setStart(String start) {
+this.start = start;
+}
+
+public End getEnd() {
+return end;
+}
+
+public void setEnd(String end) {
+this.end = end;
 }
