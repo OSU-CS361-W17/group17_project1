89bb0d2c977a55a8642ed629a16f37ca074e1cae
GarrettHaley <haleyg@oregonstate.edu>
2017-01-23 21:08:02 -0800

    Created the battleship class

--- src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
+++ src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
@@ -1,7 +1,50 @@
 package edu.oregonstate.cs361.battleship;
 
-/**
- * Created by DanielSchroederMac on 1/23/17.
- */
-public class BattleshipModel {
+public class Battleship {
+
+@SerializedName("name")
+@Expose
+private String name;
+@SerializedName("length")
+@Expose
+private Integer length;
+@SerializedName("start")
+@Expose
+private Start_ start;
+@SerializedName("end")
+@Expose
+private End_ end;
+
+public String getName() {
+return name;
+}
+
+public void setName(String name) {
+this.name = name;
+}
+
+public Integer getLength() {
+return length;
+}
+
+public void setLength(Integer length) {
+this.length = length;
+}
+
+public Start_ getStart() {
+return start;
+}
+
+public void setStart(Start_ start) {
+this.start = start;
+}
+
+public End_ getEnd() {
+return end;
+}
+
+public void setEnd(End_ end) {
+this.end = end;
+}
+
 }
