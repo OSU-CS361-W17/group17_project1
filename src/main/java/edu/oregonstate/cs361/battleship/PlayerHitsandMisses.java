4a5e52760c1792bc63c87f43c2ecfa65a4a3e41a
GarrettHaley <haleyg@oregonstate.edu>
2017-01-25 11:44:26 -0800

    Delete PlayerHitsandMisses.java

--- src/main/java/edu/oregonstate/cs361/battleship/PlayerHitsandMisses.java
+++
@@ -1,9 +0,0 @@
-package edu.oregonstate.cs361.battleship;
-public class PlayerHitsandMisses{
-
-private int [] playerHits ;
-private int [] playerMisses ;
-private int [] computerMisses ;
-private int [] computerHits ;
-
-}
